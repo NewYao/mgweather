bd763243814d45953e0ffaf3116d33485e8b215c
NewYao <jianghui_w@163.com>
2018-12-03 16:42:06 +0800

    设置底部导航栏

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,15 +1,28 @@
 //app.js
+
 App({
+  globalData:{
+    navHeight:''
+  },
   onLaunch: function () {
-    
+    wx.getSystemInfo({
+      success: res => {
+        //导航高度
+        //console.log(res.statusBarHeight + 46);
+       // this.globalData.navHeight = res.statusBarHeight + 46;
+      }, fail(err) {
+        console.log(err);
+      }
+    });
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
       wx.cloud.init({
         traceUser: true,
       })
-    }
+    };
 
-    this.globalData = {}
+    this.globalData = {
+    }
   }
 })
